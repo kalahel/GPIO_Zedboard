8e686f370fb5d7e86eabac37d632920dda7b8ac7
Mathieu Hannoun <mathieu95480@gmail.com>
2018-04-25 11:47:13 +0200

    Added Clocked reading

--- GPIO_Control.c
+++ GPIO_Control.c
@@ -17,7 +17,9 @@
 #define LINUX_OFFSET 54
 #define GPIO_OUT 0
 #define GPIO_IN 1
-
+#define EDGE_RISING 0
+#define EDGE_FALLING 1
+#define EDGE_BOTH 2
 
 int gpio_SetDataDirection(int portAddr, int mode);
 
@@ -31,17 +33,18 @@ int gpio_Write_Range(int firstPortAddr, int lastPortAddr, int valueMask[]);
 
 int gpio_Read(int portAddr, void *buf, size_t count);
 
-int gpio_Read_On_CLK(int portAddr, int clkAddr, char buf[], size_t count, char edgeOption[], size_t edgeOptionSize);
+int gpio_Clocked_Read(int portAddr, int clkAddr, char *buf, size_t count, char *edgeOption, size_t edgeOptionSize);
 
+int gpio_Clocked_Read_Simple(int portAddr, int clkAddr, char buf[], size_t count, useconds_t clkReadRate, int edgeOption);
 
-int main(int argc, char *argv[]) {
+        int main(int argc, char *argv[]) {
 
 //    int clk = 32;
 //    int data = 33;
 //    char bufferReader[20];
 //    gpio_SetDataDirection(clk, GPIO_IN);
 //    gpio_SetDataDirection(data, GPIO_IN);
-//    gpio_Read_On_CLK(33, 32, bufferReader, 20, "rising", sizeof("rising"));
+//    gpio_Clocked_Read(33, 32, bufferReader, 20, "rising", sizeof("rising"));
 //    printf("Data read : %s\r\n", bufferReader);
 //    gpio_Unexport(32);
 //    gpio_Unexport(33);
@@ -72,26 +75,54 @@ int main(int argc, char *argv[]) {
 //    gpio_Unexport(clkOut);
 //    /**/
 
-    int inPort = 32;
+    int clkInPort = 32;
+    int dataIn = 33;
     int index2;
-    char bufferReader[2];
-    gpio_SetDataDirection(inPort, GPIO_IN);
-    for (index2 = 0; index2 < 20; ++index2) {
-        gpio_Read(32, bufferReader, 2);
-        printf("Value read : %s", bufferReader);
-        sleep(1);
-    }
+    char bufferReader[10];
+    gpio_SetDataDirection(clkInPort, GPIO_IN);
+    gpio_SetDataDirection(dataIn, GPIO_IN);
+    gpio_Clocked_Read_Simple(dataIn, clkInPort, bufferReader, 10, 1000, EDGE_BOTH);
+    printf("Result value is : %s", bufferReader);
+    gpio_Unexport(32);
+    gpio_Unexport(33);
 
-    int outPort = 24;
-    gpio_SetDataDirection(outPort, GPIO_OUT);
+    //
+
+    int clkOutPort = 24;
+    int dataOutPort = 25;
+    gpio_SetDataDirection(clkOutPort, GPIO_OUT);
+    gpio_SetDataDirection(dataOutPort, GPIO_OUT);
+
     int index;
-    for (index = 0; index < 60; ++index) {
-        gpio_Write(outPort, 1);
+    for (index = 0; index < 10; ++index) {
+        gpio_Write(dataOutPort,0);
+        gpio_Write(clkOutPort, 1);
         sleep(1);
-        gpio_Write(outPort, 0);
+        gpio_Write(dataOutPort,1);
+        gpio_Write(clkOutPort, 0);
+        sleep(1);
+        gpio_Write(dataOutPort,1);
+        gpio_Write(clkOutPort, 1);
+        sleep(1);
+        gpio_Write(dataOutPort,0);
+        gpio_Write(clkOutPort, 0);
+        sleep(1);
+        gpio_Write(dataOutPort,0);
+        gpio_Write(clkOutPort, 1);
+        sleep(1);
+        gpio_Write(dataOutPort,0);
+        gpio_Write(clkOutPort, 0);
+        sleep(1);
+        gpio_Write(dataOutPort,1);
+        gpio_Write(clkOutPort, 1);
+        sleep(1);
+        gpio_Write(dataOutPort,0);
+        gpio_Write(clkOutPort, 0);
         sleep(1);
     }
-    gpio_Unexport(outPort);
+    gpio_Unexport(clkOutPort);
+    gpio_Unexport(dataOutPort);
+
 
     return EXIT_SUCCESS;
 }
@@ -164,6 +195,12 @@ int gpio_SetDataDirection(int portAddr, int mode) {
     return 0;
 }
 
+/**
+ * Unexport the selected port, good practice to unexport all used port at the end of a program
+ *
+ * @param portAddr Port to unexport
+ * @return 0 in case of success, -1 in case of failure
+ */
 int gpio_Unexport(int portAddr) {
 
 
@@ -327,7 +364,6 @@ int gpio_write_buffer(int portAddr, int valueBuffer[], int bufferSize) {
 
 /**
  * Read the data on the portAddr synchronous to the clk
- * TODO make it work
  *
  * @param portAddr
  * @param clkAddr
@@ -337,7 +373,8 @@ int gpio_write_buffer(int portAddr, int valueBuffer[], int bufferSize) {
  * @return
  */
 // TODO remove unused comment
-int gpio_Read_On_CLK(int portAddr, int clkAddr, char buf[], size_t count, char edgeOption[], size_t edgeOptionSize) {
+// TODO make it work
+int gpio_Clocked_Read(int portAddr, int clkAddr, char *buf, size_t count, char *edgeOption, size_t edgeOptionSize) {
 
     //todo add edge check
 
@@ -442,5 +479,64 @@ int gpio_Read_On_CLK(int portAddr, int clkAddr, char buf[], size_t count, char e
 
     return 0;
 
+}
 
+/**
+ * Function used to read the value on a port on each change of state on the clock
+ * Will fill the buffer with the values read
+ * Will read count - 1 value
+ *
+ * @param portAddr Address of the port to monitor
+ * @param clkAddr Address of the clock
+ * @param buf Buffer in witch the values will be put
+ * @param count Size of the buffer
+ * @param clkReadRate Refreshing rate of the reading of the clock
+ * @param edgeOption Define when to read the value, options are EDGE_BOTH, EDGE_RISING and EDGE_FALLING
+ * @return 0 in case of success
+ */
+int gpio_Clocked_Read_Simple(int portAddr, int clkAddr, char buf[], size_t count, useconds_t clkReadRate, int edgeOption) {
+
+
+    if (edgeOption != EDGE_BOTH && edgeOption != EDGE_FALLING && edgeOption != EDGE_RISING) {
+        perror("edgeOption invalid parameter\r\n");
+        exit(EXIT_FAILURE);
+    }
+
+
+    char bufferReader[2];
+    char bufferReaderDif[2];
+    char bufferReaderPortValue[2];
+    size_t index = 0;
+
+    // Read the first state of the clock
+    if (gpio_Read(clkAddr, bufferReader, 2) < 0) {
+        perror("Reading first clock value failed\r\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (index < count - 1) {
+        if (gpio_Read(clkAddr, bufferReaderDif, 2) < 0) {
+            perror("Reading clock value failed\r\n");
+            exit(EXIT_FAILURE);
+        }
+        // If the clock state as changed
+        if ((bufferReaderDif[0] != bufferReader[0])) {
+            bufferReader[0] = bufferReaderDif[0];
+            // And the edge option is checked
+            if (edgeOption == EDGE_BOTH || (edgeOption == EDGE_RISING && bufferReaderDif[0] == '1') ||
+                (edgeOption == EDGE_FALLING && bufferReaderDif[0] == '0')) {
+                // Read the value in the port monitored
+                if (gpio_Read(portAddr, bufferReaderPortValue, 2) < 0) {
+                    perror("Reading monitored port value failed\r\n");
+                    exit(EXIT_FAILURE);
+                }
+                buf[index] = bufferReaderPortValue[0];
+                index++;
+            }
+        }
+        usleep(clkReadRate);
+    }
+    buf[count - 1] = '\0';
+
+    return 0;
 }
