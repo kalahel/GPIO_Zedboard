1873956063d94aacdf62810db535933510f41bb7
Mathieu Hannoun <mathieu95480@gmail.com>
2018-05-07 10:59:46 +0200

    Added Binary data array to Writable array function

--- DirectMemoryControl.c
+++ DirectMemoryControl.c
@@ -19,12 +19,20 @@
 #define EDGE_RISING 0
 #define EDGE_FALLING 1
 #define EDGE_BOTH 2
-#define GPIO_OFFSET_OUTPUT_BANK(bankNumber) (0x0040 + (bankNumber * 0x04))
-#define GPIO_OFFSET_DIRM(bankNumber) (0x0204 + (bankNumber * 0x40))
-#define GPIO_OFFSET_OEN(bankNumber) (0x0208 + (bankNumber * 0x40))
+#define GPIO_OFFSET_OUTPUT_BANK(bankNumber) (0x0040 + ((bankNumber) * 0x04))
+#define GPIO_OFFSET_DIRM(bankNumber) (0x0204 + ((bankNumber) * 0x40))
+#define GPIO_OFFSET_OEN(bankNumber) (0x0208 + ((bankNumber) * 0x40))
 #define APER_CLK_CTRL 0x0000012C
 #define SYSTEM_LVL_CTR_BASE 0xF8000000
 #define PIN_NUMBER_PER_BANK 32
+
+
+typedef struct {
+    int nb_Data_Pins;
+    int *pins_Ports;
+    int clk_Port;
+
+} CustomMemTransceiver;
 
 
 int gpio_Mem_Set_Value(unsigned int port, int value);
@@ -47,6 +55,11 @@ int gpio_Entire_Bank_On(int bankNumber);
 
 void print_Bank_Addr();
 
+int *
+gpio_Mem_Formatting_Data(CustomMemTransceiver transceiver, const int *dataArray, int dataSize, int *resultArraySize);
+
+void print_Formatted_Data(const int *dataArray, int dataSize);
+
 // TODO make it local
 volatile unsigned int *g_CLOCK_ADDRESS;
 volatile void *g_MEMORY_MAP;
@@ -54,21 +67,33 @@ volatile void *g_MEMORY_MAP;
 int main(int argc, char *argv[]) {
 
 
-    int port = 32;
-    int fd = gpio_Mem_Map();
-    open_Amba_clk();
-    usleep(100);
-    enable_gpio_clock();
-    gpio_Mem_Set_Data_Direction(port, GPIO_OUT);
-    usleep(100);
-    //while(1){
-    gpio_Mem_Write(port, 1);
-    usleep(1);
-    //}
-    usleep(100);
-    disable_gpio_clock();
-    gpio_Mem_Unmap(fd);
+//    int port = 32;
+//    int fd = gpio_Mem_Map();
+//    open_Amba_clk();
+//    usleep(100);
+//    enable_gpio_clock();
+//    gpio_Mem_Set_Data_Direction(port, GPIO_OUT);
+//    usleep(100);
+//    //while(1){
+//    gpio_Mem_Write(port, 1);
+//    usleep(1);
+//    //}
+//    usleep(100);
+//    disable_gpio_clock();
+//    gpio_Mem_Unmap(fd);
+//
 
+
+    int data[6] = {1, 0, 1, 1, 0, 0};
+    CustomMemTransceiver transceiver;
+    int pinPorts[3] = {3, 2, 0};
+    transceiver.pins_Ports = pinPorts;
+    transceiver.nb_Data_Pins = 3;
+    int *returnedDataSize = malloc(sizeof(int));
+    *returnedDataSize = 0;
+    int *dataToPrint = gpio_Mem_Formatting_Data(transceiver, data, 6, returnedDataSize);
+    printf("Returned size : %d\r\n", *returnedDataSize);
+    print_Formatted_Data(dataToPrint, *returnedDataSize);
 
     return 0;
 }
@@ -123,6 +148,12 @@ int gpio_Mem_Set_Value(unsigned int port, int value) {
     return 0;
 }
 
+/**
+ * DEPRECATED
+ *
+ * @param bankNumber
+ * @return
+ */
 int gpio_Entire_Bank_On(int bankNumber) {
     int fd = open("/dev/mem", O_RDWR);
     volatile void *gpio_ptr = mmap(NULL, GPIO_MAP_SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, GPIO_BASE_ADDR);
@@ -267,3 +298,53 @@ int gpio_Mem_Write(int port, int value) {
     return 0;
 }
 
+/**
+ * Convert an array of binary values to an array of "Writable" values for the actual transceiver
+ * Handle the allocation of the newly created array
+ *
+ * @param transceiver Transceiver containing all the information about used ports
+ * @param dataArray Array of binary data, not '0' is treated as '1'
+ * @param dataSize Size of the data Array
+ * @param resultArraySize Size of the returned array
+ * @return The newly formatted data array
+ */
+int *
+gpio_Mem_Formatting_Data(CustomMemTransceiver transceiver, const int *dataArray, int dataSize, int *resultArraySize) {
+    int requiredRows;
+    if (dataSize % transceiver.nb_Data_Pins == 0) {
+        requiredRows = dataSize / transceiver.nb_Data_Pins;
+    }
+        // If sizes don't match, add a cell and fill the void with '0' latter
+    else {
+        requiredRows = (dataSize / transceiver.nb_Data_Pins) + 1;
+
+    }
+    int *usableDataArray = malloc(requiredRows * sizeof(int));
+    if (usableDataArray == NULL) {
+        perror("Memory allocation failed");
+        return NULL;
+    }
+    *resultArraySize = requiredRows;
+
+    int rowIndex, columnIndex;
+    unsigned int cellValue;
+    for (rowIndex = 0; rowIndex < requiredRows; rowIndex++) {
+        cellValue = 0;
+        for (columnIndex = 0; columnIndex < transceiver.nb_Data_Pins; columnIndex++) {
+            if (dataArray[columnIndex + (transceiver.nb_Data_Pins * rowIndex)] == 0) {
+                cellValue &= ~(1u << transceiver.pins_Ports[columnIndex]);
+            } else {
+                cellValue |= (1u << transceiver.pins_Ports[columnIndex]);
+            }
+        }
+        usableDataArray[rowIndex] = cellValue;
+    }
+    return usableDataArray;
+}
+
+void print_Formatted_Data(const int *dataArray, int dataSize) {
+    int index;
+    for (index = 0; index < dataSize; index++) {
+        printf("%x\r\n", dataArray[index]);
+    }
+}
